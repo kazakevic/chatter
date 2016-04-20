f2d2e35d646b01ce00edc28a8c1e4cef8685475c
kazakevic <darjus.kazakevic@gmail.com>
2016-04-20 22:33:57 +0300

    Answer search implementation

--- app/Http/Controllers/SentenceController.php
+++ app/Http/Controllers/SentenceController.php
@@ -17,34 +17,60 @@ class SentenceController extends Controller
     //
     public function Search()
     {
+        $match_count = array();
+
         //get message
         $message = Input::get('message');
+        if(!$message)
+            return;
 
 
-        //Get stop words array from database
-        $stopwords = DB::table('stopwords')->pluck('word');
         //extract keys from messagae
+        $keys = $this->getSentenceKeys($message);
 
-
-        $keys = $this->extractKeys($message, $stopwords);
         //keywords count from input message
         $keys_count = count($keys);
 
-
         //now get keys from DB by keys from message
-
-        $question = Question::all();
-
-       // foreach ($question_keys as $k)
-       // {
-       //     $question
-       // }
+        $questions = Question::all();
 
 
+        foreach ($questions as $question)
+        {
 
+            //Array of single question
+            $question_id = $question->id;
+            $q_keys = explode('; ', $question->key);
 
-        print_r($keys);
+            //how much same keys found
+            $result = array_intersect($keys, $q_keys);
+            if(count($result)>0)
+            $match_count[$question->id] = count($result);
+
+        }
+       // print_r($match_count);
+
+        //get the biggest value
+        $max_value = max($match_count);
+        //biggest value array index == question_id - that's asnwer!
+        $question_id = array_search($max_value, $match_count);
         
+        //now read asnwer from DB
+        
+        $q = Question::find($question_id);
+        $answer = Answer::find($q['answer_id']);
+
+       echo $answer['answer'];
+
+    }
+
+//returns array of sentence keys
+    private function getSentenceKeys($sentence)
+    {
+        //Get stop words array from database
+        $stopwords = DB::table('stopwords')->pluck('word');
+        return $this->extractKeys($sentence, $stopwords);
+
     }
 
 
@@ -61,7 +87,7 @@ class SentenceController extends Controller
     private function extractKeys($text, $stopwords) {
 
         // Remove line breaks and spaces from stopwords
-        $stopwords = array_map(function($x){return trim(strtolower($x));}, $stopwords);
+        $stopwords = array_map(function($x){return trim(mb_strtolower($x));}, $stopwords);
 
         // Replace all non-word chars with comma
         $pattern = '/[^\p{L}]/u';
@@ -71,7 +97,7 @@ class SentenceController extends Controller
         $text_array = explode(",",$text);
 
         // remove whitespace and lowercase words in $text
-        $text_array = array_map(function($x){return trim(strtolower($x));}, $text_array);
+        $text_array = array_map(function($x){return trim(mb_strtolower($x));}, $text_array);
 
         foreach ($text_array as $term) {
             if (!in_array($term, $stopwords)) {
