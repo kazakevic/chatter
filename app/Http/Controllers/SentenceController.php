723fee69d72140510cf37aec9c325b1a38c693e4
kazakevic <darjus.kazakevic@gmail.com>
2016-04-23 17:37:16 +0300

    Greetings checker fix, now should work correctly

--- app/Http/Controllers/SentenceController.php
+++ app/Http/Controllers/SentenceController.php
@@ -16,88 +16,7 @@ use Illuminate\Support\Facades\Input;
 
 class SentenceController extends Controller
 {
-    //
-    public function search()
-    {
-        $match_count = array();
-        //get message
-        $message = Input::get('message');
-        if (!$message)
-            return;
-
-
-
-
-        //extract keys from messagae
-        $keys = $this->getSentenceKeys($message);
-
-        //keywords count from input message
-        $keys_count = count($keys);
-        
-        if ($keys_count < Config::get('botSettings.primitiveSentenceKeys'))
-        {
-            //make search in primitive questions database
-            echo "Primitive base is empty";
-            return;
-        }
-
-
-        //now get keys from DB by keys from message
-        $questions = Question::all();
-
-
-        foreach ($questions as $question) {
-            //Array of single question
-            $question_id = $question->id;
-            $q_keys = explode('; ', $question->key);
-
-            //how much same keys found
-            $result = array_intersect($keys, $q_keys);
-            if (count($result) > 0) {
-                //array index is question ID in DB
-                $match_count[$question->id] = count($result);
-            }
-        }
-        // print_r($match_count);
-
-        //get the biggest value from result array
-        $match_count = array_filter($match_count);
-
-        if(empty($match_count)) {
-            //Send message that cant found the answer and send report
-            //Save message if it's question
-            echo "What? I don't understand...";
-            return;
-        }
-
-        $max_value = max($match_count);
-
-
-        if($max_value < Config::get('botSettings.minKeysToMatch'))
-        {
-            echo "I have no answer";
-            return;
-        }
-        //biggest value array index == question_id - that's asnwer!
-        $question_id = array_search($max_value, $match_count);
-
-        //now read asnwer from DB
-
-        $q = Question::find($question_id);
-        $answer = Answer::find($q['answer_id']);
-
-        $answer = $answer['answer'];
-
-        //check if this first question and if it have greeting key
-        if($this->checkForGreeting($message))
-            $answer = "Laba diena :) ".$answer;
-        
-        //Generate and output asnwer
-        echo $answer;
-
-    }
-
-//returns array of sentence keys
+    //returns array of sentence keys
     private function getSentenceKeys($sentence)
     {
         //Get stop words array from database
@@ -108,24 +27,37 @@ class SentenceController extends Controller
 
     private function checkIfQuestion($message)
     {
-        if (preg_match('/\?/',$message))
+        if (preg_match('/\?/', $message))
             return true;
     }
+
     private function contains($str, array $arr)
     {
-        foreach($arr as $a) {
+        foreach ($arr as $a) {
             if (stripos($str, $a) !== false) return true;
         }
         return false;
     }
+
     private function checkForGreeting($msg)
     {
+        $keys = array();
         $greeting = Identificator::where('identity', 'greeting')->get();
-        
-        if(contains($msg, $greeting))
-        return true;
+        foreach ($greeting as $g) {
+            $keys[] = $g->key;
+        }
+
+        if ($this->contains($msg, $keys))
+            return true;
         else
-        return false;
+            return false;
+    }
+
+    private function generateGreeting()
+    {
+        $greetings = array("Laba diena.", "Sveiki.", "Gerą dieną.");
+        $k = array_rand($greetings);
+        return $greetings[$k];
     }
 
     private function extractKeys($text, $stopwords)
@@ -155,6 +87,78 @@ class SentenceController extends Controller
 
 //array of UNIQUE keywoards without stopwords
         return array_filter(array_unique($keywords));
+    }
+
+    //
+    public function search()
+    {
+        $match_count = array();
+        //get message
+        $message = Input::get('message');
+        if (!$message)
+            return;
+        
+        //extract keys from messagae
+        $keys = $this->getSentenceKeys($message);
+
+        //keywords count from input message
+        $keys_count = count($keys);
+
+
+        //now get keys from DB by keys from message
+        $questions = Question::all();
+
+
+        foreach ($questions as $question) {
+            //Array of single question
+            $question_id = $question->id;
+            $q_keys = explode('; ', $question->key);
+
+            //how much same keys found
+            $result = array_intersect($keys, $q_keys);
+            if (count($result) > 0) {
+                //array index is question ID in DB
+                $match_count[$question->id] = count($result);
+            }
+        }
+        // print_r($match_count);
+
+        //get the biggest value from result array
+        $match_count = array_filter($match_count);
+
+        if (empty($match_count)) {
+            //Send message that cant found the answer and send report
+            //Save message if it's question
+            echo "What? I don't understand...";
+            return;
+        }
+
+        $max_value = max($match_count);
+
+
+        if ($max_value < Config::get('botSettings.minKeysToMatch')) {
+            echo "I have no answer";
+            return;
+        }
+        //biggest value array index == question_id - that's asnwer!
+        $question_id = array_search($max_value, $match_count);
+
+        //now read asnwer from DB
+
+        $q = Question::find($question_id);
+        $answer = Answer::find($q['answer_id']);
+
+        $answer = $answer['answer'];
+
+        //check if this first question and if it have greeting key
+        if ($this->checkForGreeting($message))
+            $answer = $this->generateGreeting() . $answer;
+        else
+            $answer = $answer;
+
+        //Generate and output asnwer
+        echo $answer;
+
     }
 
 
