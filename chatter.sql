da04793b438dcbf1e5b559ec43b08b04f44ac273
kazakevic <darjus.kazakevic@gmail.com>
2016-04-27 23:04:02 +0300

    DB update

--- chatter.sql
+++ chatter.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2016-04-27 16:02:40
+Date: 2016-04-27 23:03:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,17 +22,18 @@ DROP TABLE IF EXISTS `answers`;
 CREATE TABLE `answers` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `answer` mediumtext NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of answers
 -- ----------------------------
-INSERT INTO `answers` VALUES ('1', 'Į aviakompanijos Ryanair aviabilieto kainą įskaičiuotas rankinis bagažas (1 bagažo vienetas iki 10 kg), kurį nešatės su savimi į lėktuvo saloną. Galima nemokamai į lėktuvą įsinešti ir antrą bagažą – mažą moterišką rankinę – bei oro uoste įsigytas prekes. Antroji rankinė negali viršyti 35 x 20 x 20 cm išmatavimų. Antrąjį bagažo vienetą reikės pasidėti lentynose virš sėdimųjų vietų arba po kėde.');
-INSERT INTO `answers` VALUES ('2', 'Rankinis bagažas – 1 bagažo vienetas iki 10 kg ir maža moteriška rankinė. Rankinio bagažo išmatavimai negali viršyti 55 x 40 x 20 cm. Antrasis bagažas negali viršyti 35 x 20 x 20 cm išmatavimų. Mažajai bagažui svorio apribojimai netaikomi. Atkreipkite dėmesį į bagažo taisykles.');
-INSERT INTO `answers` VALUES ('3', 'Aviakompanijos Ryanair registruojamas bagažas gali būti iki 15 kg arba iki 20 kg (išmatavimai 81 x 119 x 119 cm). Vienas keleivis gali gabenti iki 3 registruoto Ryanair bagažo vienetų.\r\nKeleiviai, esantys vienoje rezervacijoje ir kartu užsiregistravę į Ryanair skrydį, gali dalintis registruoto bagažo apribojimais. Pvz., 2 keleiviai, esantys vienoje rezervacijoje, įsigiję du registruotus bagažus po 15 kg, gali pasidalinti svoriu. Vienas keleivis gali turėti registruotą bagažą, kuris sveria 17 kg, o kitas – 13 kg.');
-INSERT INTO `answers` VALUES ('4', 'Skaitau knygą.');
-INSERT INTO `answers` VALUES ('5', '');
+INSERT INTO `answers` VALUES ('1', 'Į aviakompanijos Ryanair aviabilieto kainą įskaičiuotas rankinis bagažas (1 bagažo vienetas iki 10 kg), kurį nešatės su savimi į lėktuvo saloną. Galima nemokamai į lėktuvą įsinešti ir antrą bagažą – mažą moterišką rankinę – bei oro uoste įsigytas prekes. Antroji rankinė negali viršyti 35 x 20 x 20 cm išmatavimų. Antrąjį bagažo vienetą reikės pasidėti lentynose virš sėdimųjų vietų arba po kėde.', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `answers` VALUES ('2', 'Rankinis bagažas – 1 bagažo vienetas iki 10 kg ir maža moteriška rankinė. Rankinio bagažo išmatavimai negali viršyti 55 x 40 x 20 cm. Antrasis bagažas negali viršyti 35 x 20 x 20 cm išmatavimų. Mažajai bagažui svorio apribojimai netaikomi. Atkreipkite dėmesį į bagažo taisykles.', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `answers` VALUES ('3', 'Aviakompanijos Ryanair registruojamas bagažas gali būti iki 15 kg arba iki 20 kg (išmatavimai 81 x 119 x 119 cm). Vienas keleivis gali gabenti iki 3 registruoto Ryanair bagažo vienetų.\r\nKeleiviai, esantys vienoje rezervacijoje ir kartu užsiregistravę į Ryanair skrydį, gali dalintis registruoto bagažo apribojimais. Pvz., 2 keleiviai, esantys vienoje rezervacijoje, įsigiję du registruotus bagažus po 15 kg, gali pasidalinti svoriu. Vienas keleivis gali turėti registruotą bagažą, kuris sveria 17 kg, o kitas – 13 kg.', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `answers` VALUES ('4', 'Skaitau knygą. O tu?', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- ----------------------------
 -- Table structure for identificators
@@ -43,7 +44,7 @@ CREATE TABLE `identificators` (
   `key` varchar(255) DEFAULT NULL,
   `identity` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of identificators
@@ -80,7 +81,7 @@ CREATE TABLE `questions` (
   `length` int(11) DEFAULT '0',
   `main_key` varchar(255) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of questions
@@ -90,7 +91,9 @@ INSERT INTO `questions` VALUES ('2', 'Koks yra Ryanair rankinio bagažo svoris v
 INSERT INTO `questions` VALUES ('3', 'Koks yra Ryanair registruojamo bagažo svoris vienam keleiviui?', 'registruojamo; keleiviui; ryanair', '3', '0000-00-00 00:00:00', '2016-04-25 16:46:24', '3', 'registruojamo');
 INSERT INTO `questions` VALUES ('4', 'Ką veiki?', 'veiki; ką', '4', '0000-00-00 00:00:00', '2016-04-25 22:43:51', '2', 'veiki');
 INSERT INTO `questions` VALUES ('5', 'Ką tu?', 'ką; tu', '4', '0000-00-00 00:00:00', '2016-04-25 22:43:51', '2', 'ką');
-INSERT INTO `questions` VALUES ('6', 'Kas tu esi?', '0', '0', '2016-04-27 12:55:28', '2016-04-27 12:55:28', '0', '0');
+INSERT INTO `questions` VALUES ('11', 'kas tai namas?', '0', '0', '2016-04-27 19:53:32', '2016-04-27 19:53:32', '0', '0');
+INSERT INTO `questions` VALUES ('12', 'kas tai f22?', '0', '0', '2016-04-27 19:59:59', '2016-04-27 19:59:59', '0', '0');
+INSERT INTO `questions` VALUES ('13', 'kas tai wall street?', '0', '0', '2016-04-27 20:00:14', '2016-04-27 20:00:14', '0', '0');
 
 -- ----------------------------
 -- Table structure for shot_questions
