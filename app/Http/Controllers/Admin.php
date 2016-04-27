f202c064ee92fc3974e86de78c6e42cb430d6421
kazakevic <darjus.kazakevic@gmail.com>
2016-04-27 19:22:01 +0300

    Forgot Admin controller :(

---
+++ app/Http/Controllers/Admin.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Question;
+use App\Answer;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+
+class Admin extends Controller
+{
+    //
+    public function showQuestions()
+    {
+        $data = array(
+            'questions' => Question::where('answer_id', 0)->get()
+        );
+        return view('admin/questions', $data);
+
+    }
+    public function answer($id)
+    {
+        $data = array(
+            'qid' => $id
+        );
+        return view('admin/writeAnswer', $data);
+    }
+    public function answerSave()
+    {
+        $answer = Input::get('answer');
+        $q_id = Input::get('qid');
+
+        $ans = new Answer();
+        $ans->answer = $answer;
+        $ans->save();
+
+        $q = Question::where('id', $q_id)->first();
+        $q->answer_id = $ans->id;
+        $q->save();
+
+        return redirect('admin/questions');
+
+
+    }
+
+}
